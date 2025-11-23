bc1a1158158d8a6bf12eb8ace7db955f679805dd
Thomas <34007164+kai7win@users.noreply.github.com>
2025-11-23 14:34:15 +0800

    Initial Commit

---
+++ AnimatedGlassTabs-UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimatedGlassTabs-UIKit
+//
+//  Created by Thomas on 2025/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
